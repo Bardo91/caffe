13335830f2811d78ecbf4c2b62c57493817d164e
Bardo91 <19Bardo91@gmail.com>
2017-05-31 21:59:26 +0200

    Added example project using SSD

---
+++ examples/ssd/use_detect/src/WrapperSSD.h
@@ -0,0 +1,37 @@
+//
+//
+//
+//
+//
+//
+
+#include <string>
+
+#include <opencv2/opencv.hpp>
+
+#include <caffe/caffe.hpp>
+
+
+class WrapperSSD{
+public:
+    WrapperSSD(const std::string& model_file,
+             const std::string& weights_file,
+             const std::string& mean_file,
+             const std::string& mean_value);
+
+    std::vector<std::vector<float> > Detect(const cv::Mat& img);
+
+    private:
+    void SetMean(const std::string& mean_file, const std::string& mean_value);
+
+    void WrapInputLayer(std::vector<cv::Mat>* input_channels);
+
+    void Preprocess(const cv::Mat& img,
+                    std::vector<cv::Mat>* input_channels);
+
+private:
+    std::shared_ptr<caffe::Net<float> > net_;
+    cv::Size input_geometry_;
+    int num_channels_;
+    cv::Mat mean_;
+};
